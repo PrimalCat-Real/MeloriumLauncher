9be1f7680544d345b4cf902d4658f0cf898c4d00
PrimalCat-Real <romanenko.denis2003@gmail.com>
2025-08-06 20:46:21 +0300

    change logo size

--- src/components/header/LogoBrand.tsx
+++ src/components/header/LogoBrand.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const LogoBrand = ({width = 36, height = 36}: {width?: number, height?: number}) => {
+const LogoBrand = ({width = 30, height = 30}: {width?: number, height?: number}) => {
   return (
     <svg width={width} height={height} viewBox="0 0 57 63" fill="inherit" xmlns="http://www.w3.org/2000/svg">
         <desc>
